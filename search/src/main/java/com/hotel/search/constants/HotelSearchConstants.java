3d709f8e15e5ff9d3555c2879480aa54c00eeae2
venu-sagar <sagar.venu@fenecon.de>
2020-09-01 18:25:55 +0200

    Constants

---
+++ search/src/main/java/com/hotel/search/constants/HotelSearchConstants.java
@@ -0,0 +1,9 @@
+package com.hotel.search.constants;
+
+public class HotelSearchConstants {
+    public static final String hotelsPath= "/csv/hotels.csv";
+    public static final String advertisersPath= "/csv/advertisers.csv";
+    public static final String hotelAdvertisersPath= "/csv/hotel_advertiser.csv";
+    public static final String citiesPath= "/csv/cities.csv";
+
+}
